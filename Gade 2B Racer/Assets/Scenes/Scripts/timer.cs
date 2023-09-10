d06cfca310ef190e989ff382f088f7f696140350
LebohangMatlosa <88308153+LebohangMatlosa@users.noreply.github.com>
2023-09-11 00:33:40 +0200

    Coroutine

--- Gade 2B Racer/Assets/Scenes/Scripts/timer.cs
+++ Gade 2B Racer/Assets/Scenes/Scripts/timer.cs
@@ -2,15 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 public class timer : MonoBehaviour
 {
     public TextMeshProUGUI timerText;
     private float startTime;
     private bool isRaceActive = false;
+    public Button startTimeButton;
+    public Button stopTimeButton;
+    IEnumerator _timer;
 
-    void Start()
+    void TimerReset()
     {
+        startTimeButton.interactable = true;
+        stopTimeButton.interactable = false;
         timerText.text = "Time: 0.00";
+    }
+
+    private void Awake()
+    {
+        startTimeButton.onClick.AddListener(StartTimerOnClick);
+        stopTimeButton.onClick.AddListener(StopTimerOnClick);
+        TimerReset();
+    }
+
+    void StartTimerOnClick()
+    {
+        _timer = BeginTimer();
+        StartCoroutine(_timer);
+    }
+
+    void StopTimerOnClick()
+    {
+        if(_timer != null)
+        {
+            StopCoroutine(_timer);
+            _timer = null;
+        }
+        TimerReset();
+    }
+    IEnumerator BeginTimer(int remainingTime = 30)
+    {
+        startTimeButton.interactable = false;
+        stopTimeButton.interactable = true;
+        for (int i = remainingTime; i > 0; i--)
+        {
+            timerText.text = i.ToString("00");
+            yield return new WaitForSeconds(1);
+        }
+        TimerReset();
     }
 
     void Update()
