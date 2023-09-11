49e26ec00eeaeee34bb9f2dcd9759c4ab2a90f2b
RaeezLachporia <88308343+RaeezLachporia@users.noreply.github.com>
2023-09-11 16:41:42 +0200

    Added scene management
    Added the feature to switch between the scenes to get to the various screens in the game

---
+++ Gade 2B Racer/Assets/Scenes/Scripts/Dialogue/BeginnerRace.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BeginnerRace : MonoBehaviour
+{
+    [SerializeField] private Queue<string> dialogue;
+    [SerializeField] private TextMeshProUGUI display;
+    void Start()
+    {
+        DialogueForCheeckpoint();
+    }
+     
+    void DialogueForCheeckpoint()
+    {
+        dialogue = new Queue<string>();
+        dialogue.Enqueue("Hello");
+        dialogue.Enqueue("Welcome to the time trial race");
+        dialogue.Enqueue("You will have to beat the race and cross the finish line before the timer runs out");
+        dialogue.Enqueue("To start press the start timer button when you are ready to go ");
+        dialogue.Enqueue("Good Luck!!");
+    }
+
+    public void nextSentence()
+    {
+        display.text = dialogue.Peek();
+        dialogue.Dequeue();
+    }
+}
---
+++ Gade 2B Racer/Assets/Scenes/Scripts/MenuScripts/menuController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class menuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GoToRaceScreen()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitApp()
+    {
+        Application.Quit();
+    }
+
+    public void LoadRaceDialogue()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadTimeTrialRace()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+}
