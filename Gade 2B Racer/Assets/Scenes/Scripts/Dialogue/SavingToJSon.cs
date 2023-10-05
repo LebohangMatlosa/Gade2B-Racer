963a02247f5471bdc02ad5eb6fc54f5182665d13
RaeezLachporia <88308343+RaeezLachporia@users.noreply.github.com>
2023-10-05 10:19:36 +0200

    trying to save to JSOn

---
+++ Gade 2B Racer/Assets/Scenes/Scripts/Dialogue/SavingToJSon.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using TMPro;
+[System.Serializable]
+public class SavingToJSon : MonoBehaviour
+{
+
+    string NameText;
+    string DialogueText;
+
+    
+}
