6e120e35bfe056cbed10a0ca63652f9642004b57
RaeezLachporia <88308343+RaeezLachporia@users.noreply.github.com>
2023-10-05 15:05:41 +0200

    Finally saved to JSON file
    saved the text to the Json

--- Gade 2B Racer/Assets/Scenes/Scripts/Dialogue/BeginnerRace.cs
+++ Gade 2B Racer/Assets/Scenes/Scripts/Dialogue/BeginnerRace.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System.IO;
-
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Linq;
 public class BeginnerRace : MonoBehaviour
 {
-    [SerializeField] private Queue<string> dialogue;
+    [SerializeField] private SerilizableDialogue<string> dialogue;
     [SerializeField] public string Announcer;
     [SerializeField] private TextMeshProUGUI display;
+    
     public GameObject Text;
     void Start()
     {
@@ -16,20 +19,38 @@ public class BeginnerRace : MonoBehaviour
         Text.GetComponent<TextMeshProUGUI>().text = "Raeez";
     }
      
-    void DialogueForCheeckpoint()
+    public void DialogueForCheeckpoint()
     {
         
-        dialogue = new Queue<string>();
-        dialogue.Enqueue("Hello");
-        dialogue.Enqueue("Welcome to the time trial race");
-        dialogue.Enqueue("You will have to beat the race and cross the finish line before the timer runs out");
-        dialogue.Enqueue("To start press the start timer button when you are ready to go ");
-        dialogue.Enqueue("Good Luck!!");
+        //dialogue = new Queue<string>();
+        dialogue.queue.Enqueue("Hello");
+        dialogue.queue.Enqueue("Welcome to the time trial race");
+        dialogue.queue.Enqueue("You will have to beat the race and cross the finish line before the timer runs out");
+        dialogue.queue.Enqueue("To start press the start timer button when you are ready to go ");
+        dialogue.queue.Enqueue("Good Luck!!");
+        dialogue.list = new List<string>(dialogue.queue);
+        var Json = JsonUtility.ToJson(dialogue);
+        
+        Debug.Log(Json + "converted");
+        System.IO.File.WriteAllText(Application.persistentDataPath + "/DialogueStorage.json",Json);
+        Debug.Log(Application.persistentDataPath + "/DialogueStorage.json");
     }
 
     public void nextSentence()
     {
-        display.text = dialogue.Peek();
-        dialogue.Dequeue();
+        display.text = dialogue.queue.Peek();
+        dialogue.queue.Dequeue();
     }
+    [System.Serializable]
+    public class SerilizableDialogue<T>
+    {
+        public Queue<T> queue = new Queue<T>();
+
+        public List<T> list;
+
+
+        
+    }
+
+    
 }
