6a83ca17bc5defed14f0d6aae978535705c5e3b7
RaeezLachporia <88308343+RaeezLachporia@users.noreply.github.com>
2023-10-29 19:00:21 +0200

    fixing things with alice

--- Gade 2B Racer/Assets/Scenes/Scripts/Dialogue/DialogueManager.cs
+++ Gade 2B Racer/Assets/Scenes/Scripts/Dialogue/DialogueManager.cs
@@ -9,63 +9,74 @@ public class DialogueManager : MonoBehaviour
     public TextMeshProUGUI displayName;
     public TextMeshProUGUI displayDialogue;
     public Queue<string> dialogueSentences;
+    private Queue<nonPlayer> npcCharacters;
     public TextAsset jsonFile;
     // Start is called before the first frame update
     void Start()
     {
+        // Initializes the queue
         dialogueSentences = new Queue<string>();
-        NonPlayerCharacters announcerInJSON = JsonUtility.FromJson<NonPlayerCharacters>(jsonFile.text);
-        foreach (nonPlayer announcer in announcerInJSON.npcCharacters)
-        {
-            Debug.Log("Found announcer: " + announcer.npcName + "'" + announcer.npcDialogue + "'");
-            displayName.text = announcer.npcName;
-            //dialogueSentences.Clear();
-            dialogueSentences.Enqueue(announcer.npcDialogue);
-            
+        npcCharacters = new Queue<nonPlayer>();
+        NonPlayerCharacters characInJSON = JsonUtility.FromJson<NonPlayerCharacters>(jsonFile.text);
 
+        foreach (nonPlayer character in characInJSON.npcCharacters)
+        {
+            Debug.Log("Found NPC: " + character.npcName + "'" + character.npcDialogue + "'");
+            npcCharacters.Enqueue(character);
         }
-        displayNextSentence();
-    }
 
-    public void startDialogue(nonPlayer announcerDialogue)
-    {
-        Debug.Log("Starting conversation with " +announcerDialogue.npcName);
-        /*displayName.text = announcerDialogue.announcerName;
-        dialogueSentences.Clear();*/
-
-        /*foreach (var dialogueSentence in announcerDialogue.announcerName)
+        // Start the dialogue with the first character
+        if (npcCharacters.Count > 0)
         {
-            //dialogueSentences.Enqueue(dialogueSentence);
-        }*/
+            startDialogue(npcCharacters.Peek()); // Peek at the first character without dequeuing
+        }
+    }
+
+    // this takes in a dialogue parameter and starts the dialogue 
+    public void startDialogue(nonPlayer npcDialogue)
+    {
+        Debug.Log("starting convo with " + npcDialogue.npcName);
+        displayName.text = npcDialogue.npcName; // displays the name
+        dialogueSentences.Clear(); // clears out the queue for new data to come in
+
+        // loops through the array and enqueues each sentence in the array
+        foreach (var dialogueSentence in npcDialogue.npcDialogue)
+        {
+            dialogueSentences.Enqueue(npcDialogue.npcDialogue);
+        }
+
+        // Display the first sentence
         displayNextSentence();
     }
 
+    // displays the next queued sentence
     public void displayNextSentence()
     {
-        if (dialogueSentences.Count==0)
+        if (dialogueSentences.Count == 0) // checks that if there are any queued items left
         {
+            // End the dialogue for the current character
             endDialogue();
-            return;
-        }
-        string sentence = dialogueSentences.Dequeue();
-        Debug.Log(sentence);
-        //StopAllCoroutine();
-        //StartCoroutine(typeSentence(sentence));
 
-        /*IEnumerator typeSentence(string sentence)
-        {
-            displayDialogue.text = "";
-            foreach (char letter in sentence.ToCharArray())
+            // Dequeue the current character and start the next character's dialogue
+            npcCharacters.Dequeue();
+
+            if (npcCharacters.Count > 0)
             {
-                displayDialogue.text += letter;
-                yield return new WaitForSeconds(0.15f);
+                startDialogue(npcCharacters.Peek());
             }
-        }*/
+        }
+        else
+        {
+            string sentence = dialogueSentences.Dequeue(); // removes the displayed sentence from the queue
+            Debug.Log(sentence);
+            StopAllCoroutines(); // this ensures that the current sentence finishes before the next
+
+        }
     }
 
     public void endDialogue()
     {
-        Debug.Log("End Of Convo");
+        Debug.Log("end of convo");
     }
-   
 }
+
