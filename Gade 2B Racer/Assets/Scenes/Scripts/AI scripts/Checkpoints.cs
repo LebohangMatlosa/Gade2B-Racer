5c65dd45d2388c7bfdcd486bcead452fe4ef669a
RaeezLachporia <88308343+RaeezLachporia@users.noreply.github.com>
2023-10-30 13:55:32 +0200

    WorkingNaveMeshCheckpoints

--- Gade 2B Racer/Assets/Scenes/Scripts/AI scripts/Checkpoints.cs
+++ Gade 2B Racer/Assets/Scenes/Scripts/AI scripts/Checkpoints.cs
@@ -7,4 +7,12 @@ public class Checkpoints : MonoBehaviour
     public Transform checkpointPosition;
     public Checkpoints nextCheckpoint;
     public Checkpoints previousCheckpoint;
+    private Vector3 position;
+    private int checkpointID;
+
+    public Checkpoints(Vector3 position, int checkpointID)
+    {
+        this.position = position;
+        this.checkpointID = checkpointID;
+    }
 }
